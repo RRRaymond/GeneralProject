e5d8ae841d63238c7e7f68e28601d52eca3a4698
LAPTOP-R1O35NRO\Angel <zhuanqi97@tongji.edu.cn>
2018-04-19 23:31:18 +0800

    添加两种组合障碍物且留出接口

--- Assets/Scripts/Game2/Game2_blockSpawn.cs
+++ Assets/Scripts/Game2/Game2_blockSpawn.cs
@@ -8,6 +8,13 @@ public class Game2_blockSpawn : MonoBehaviour {
 	public Transform m_block_square;
 	protected Transform m_transform;
 
+	//parameters
+	public int interval_ms = 300;
+	public int interval_range = 200;
+	public float interval_long_short = 1.0f;
+	public float interval_long_long = 2.0f;
+
+
 	int cnt = 0;
 
 	// Use this for initialization
@@ -18,26 +25,69 @@ public class Game2_blockSpawn : MonoBehaviour {
 
 	IEnumerator SpawnBlock()
 	{
-		double maxstep0 = (100.0 / ((cnt / 10) + 1) + 300);
+		double maxstep0 = (interval_range / ((cnt / 10) + 1.0) + interval_ms);
 		int maxstep = (int)(maxstep0);
-		float timestep = Random.Range(200, maxstep) / 100;
+		float timestep = Random.Range(300, maxstep) / 100;
 		yield return new WaitForSeconds (timestep);
 
 		Vector3 new_postion = m_transform.position;
 
 		int temp = Random.Range (1, 10000);
-		new_postion.x = (float)(temp % 800 - 400)/(float)100.0;
+		new_postion.x = (float)(temp % 600 - 200)/(float)100.0;
 		new_postion.y = 8;
 
+		int types = 5;
 		int temp2 = Random.Range (1, 15);
-		Transform temp_t;
-		if (temp2 % 3 == 0)
-			temp_t = m_block_long;
-		else if(temp2 % 3 == 1)
-			temp_t = m_block_short;
-		else
-			temp_t = m_block_square;
-		Object cloneObj = Instantiate (temp_t, new_postion, Quaternion.identity);
+		if (temp2 % types == 0) {
+			Transform temp_t = m_block_long;
+			Object cloneObj = Instantiate (temp_t, new_postion, Quaternion.identity);
+		} else if (temp2 % types == 1) {
+			Transform temp_t = m_block_short;
+			Object cloneObj = Instantiate (temp_t, new_postion, Quaternion.identity);
+		} else if (temp2 % types == 2) {
+			Transform temp_t = m_block_square;
+			Object cloneObj = Instantiate (temp_t, new_postion, Quaternion.identity);
+		} else if (temp2 % types == 3) {
+			Transform temp_t1 = m_block_long;
+			Transform temp_t2 = m_block_short;
+			int temp3 = Random.Range (1, 15);
+			Random.Range (1, 15);
+			Vector3 pos1 = m_transform.position;
+			Vector3 pos2 = m_transform.position;
+			pos1.x = -2;
+			pos1.y = 8;
+			if (temp3 % 2 == 0) {
+				pos2.x = 2.5f;
+				pos2.y = 8;
+				Object cloneObj1 = Instantiate (temp_t1, pos1, Quaternion.identity);
+				yield return new WaitForSeconds (interval_long_short);
+				Object cloneObj2 = Instantiate (temp_t2, pos2, Quaternion.identity);
+			} else {
+				pos2.x = 2;
+				pos2.y = 8;
+				Object cloneObj1 = Instantiate (temp_t1, pos2, Quaternion.identity);
+				yield return new WaitForSeconds (interval_long_short);
+				Object cloneObj2 = Instantiate (temp_t2, pos1, Quaternion.identity);
+			}
+		} else if (temp2 % types == 4) {
+			int temp3 = Random.Range (1, 4);
+			int temp4 = Random.Range (1, 15);
+			Vector3 pos1 = m_transform.position;
+			pos1.y = 8;
+			if (temp4 % 2 == 0)
+				pos1.x = -2;
+			else
+				pos1.x = 1;
+			Transform temp_t1 = m_block_long;
+			Object cloneObj1 = Instantiate (temp_t1, pos1, Quaternion.identity);
+			while (temp3--!=0) {
+				yield return new WaitForSeconds (interval_long_long);
+				Object cloneObj = Instantiate (temp_t1, pos1, Quaternion.identity);
+			}
+
+		}
+
+
 		cnt++;
 		StartCoroutine (SpawnBlock());
 	}
